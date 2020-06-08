62ba904fb74af1be95ee2fcff82164442355eb74
Devon Merrill <djmerrill@github.com>
2020-06-08 10:48:42 -0700

    better reporting

--- ser_test.py
+++ ser_test.py
@@ -11,9 +11,10 @@ while True:
     line = ser.readline();
     line_count += 1
 
-    if line_count % 50 == 0:
+    if line_count % 500 == 0:
         current_time = time.time()
         if current_time - start_time > 1.0:
             print('Got ' + str(line_count) + ' lines in ' + str(current_time - start_time))
             print('\t' + str(line_count / (current_time - start_time)) + ' lines per second')
+            print('\t' + str(line_count / 6 / (current_time - start_time)) + ' samples per sensor per second')
             print('\tLast line: ' + str(line))
