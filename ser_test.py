7e0551337ee2306d5b38b5e8f84569e958846541
Devon <djmerrill@ucsd.edu>
2020-06-14 19:13:15 -0700

    refactor for multiple ports and auto detection of ports

--- ser_test.py
+++ ser_test.py
@@ -8,8 +8,6 @@ import time
 import serial
 
 
-
-
 # Save the data from band
 # The file name should be a command line option
 def save_data(data, filename):
@@ -25,11 +23,15 @@ def save_data(data, filename):
                 f.write(str(d) + ' ')
 
 
+sers = []
+active_ports = 0
 
-
-ser0 = serial.Serial('/dev/ttyACM0') # TODO This port name should be a parameter. 
-ser1 = serial.Serial('/dev/ttyACM1') # TODO This port name should be a parameter. 
-#ser2 = serial.Serial('/dev/ttyACM2') # TODO This port name should be a parameter. 
+for i in range(4):
+    try:
+        sers.append(serial.Serial('/dev/ttyACM' + str(i))) # TODO This port name should be a parameter. 
+        active_ports += 1
+    except:
+        sers.append(None)
 
 # I know this is weird, but this remapping is so the Arduino serial functions work correctly
 # These value can change if needed
@@ -37,16 +39,16 @@ ser1 = serial.Serial('/dev/ttyACM1') # TODO This port name should be a parameter
 zero_char = 64
 newline_char = 65
 
-line_count = 0
-frame_count = 0
-start_time = None
 
-_ = ser0.readline()
-_ = ser1.readline()
+# attempt to syncronize
+for ser in sers:
+    if ser is not None:
+        _ = ser.readline()
 
-
-ser0_data = []
-ser1_data = []
+# lists for data
+datas = []
+for ser in sers:
+    datas.append([])
 
 def check_end_frame(data):
     if len(data) < 3:
@@ -60,40 +62,39 @@ def check_data(data):
         data.append(newline_char)
         data.append(newline_char)
 
+line_count = 0
+frame_count = 0
+start_time = None
 
 # The break condition should be time based and set using a command line option
-while len(ser0_data) < 100000:
+while frame_count/active_ports < 10000:
 
-    # record start time, the Arduino might not start right away so we should wait for the first good read
-    if start_time is None:
-        start_time = time.time()
-    line = ser0.read(16);
-    assert len(line) == 16
 
-    # I'm just saving it like this for testing
-    for d in line:
-        if check_end_frame(ser0_data):
-            frame_count += 1
-        check_data(ser0_data)
-        ser0_data.append(d)
+    for i, ser in enumerate(sers):
+        if ser is None:
+            continue
+        # record start time, the Arduino might not start right away so we should wait for the first good read
+        if start_time is None:
+            start_time = time.time()
+        line = ser.read(16);
+        assert len(line) == 16
+
+        # I'm just saving it like this for testing
+        for d in line:
+            if check_end_frame(datas[i]):
+                frame_count += 1
+            check_data(datas[i])
+            datas[i].append(d)
         
 
-    # I think this will really read and take time, even if the data is not used. So it's ok for testing
-    line1 = ser1.read(16);
-    for d in line1:
-        check_data(ser1_data)
-        ser1_data.append(d)
-    #line2 = ser2.read(4);
-    
-
-    if line_count % 500 == 0:
+    if line_count % 500*active_ports == 0:
     #if True:
         current_time = time.time()
         if current_time - start_time > 1.0:
         #if True:
             print('Got ' + str(frame_count) + ' frames in ' + str(current_time - start_time))
             print('\t' + str(frame_count / (current_time - start_time)) + ' frames per second')
-            print('\t' + str(frame_count / (current_time - start_time)) + ' samples per sensor per second')
+            print('\t' + str(frame_count / active_ports / (current_time - start_time)) + ' samples per sensor per second')
             print('\tLast line: ' + str([str(d) for d in line]))
 
 visualize = False
@@ -108,5 +109,5 @@ if visualize:
     for i, c in enumerate(counts):
         print(str(i) + ': ' + str(c))
 
-save_data(ser0_data, '0_data.txt')
-save_data(ser1_data, '1_data.txt')
+for i, data in enumerate(datas):
+    save_data(data, str(i) + '_data.txt')
